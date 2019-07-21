c17a5475a86d3824aaf499901006828964053eeb
wangpeng01 <wangpeng01@aiyunxiao.com>
2019-07-21 14:20:42 +0800

    3

--- startFFMPEG.js
+++ startFFMPEG.js
@@ -1,8 +1,7 @@
 var process = require('child_process');
 
 function start() {
-    var shell = 'ffmpeg -i "rtsp://admin:tyzx2013@61.233.3.221/" -f flv -r 25 -s 640*360 -an "rtmp:localhost:1935/live/test"';
-    console.log(shell)
+    var shell = 'ffmpeg -rtsp_transport tcp -i "rtsp://admin:tyzx2013@61.233.3.221/" -f flv -r 25 -s 640*360 -b 10M -an "rtmp:localhost:1935/live/test"';
     process.exec(shell,function (error, stdout, stderr) {
         if (error !== null) {
           console.log('exec error: ' + error);
